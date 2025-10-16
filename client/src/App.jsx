98bdaa24ce1e8bc05c040c2b0df512c562ab0d33
omar <omarqais225@gmail.com>
2025-10-16 16:49:34 +0300

    create themes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,9 +3,11 @@ import Navbar from './components/Navbar';
 import { Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import ProductPage from './pages/ProductPage';
+import { useThemeStore } from '../store/useThemeStore';
 const App = () => {
+  const { theme } = useThemeStore();
   return (
-    <div className='min-h-screen bg-base-200 transition-colors duration-300' data-theme="forest">
+    <div className='min-h-screen bg-base-200 transition-colors duration-300' data-theme={theme}>
       <Navbar />
       <Routes>
         <Route path='/' element={<HomePage />} />
