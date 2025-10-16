19c6b0f5bd36f4f9a6538e67d453592e54004114
omar <omarqais225@gmail.com>
2025-10-16 15:45:18 +0300

    build arcjet for security

--- server/server.js
+++ server/server.js
@@ -7,18 +7,47 @@ import { response, request } from 'express';
 import cors from 'cors';
 import productsRoutes from './routes/productsRoutes.js';
 import { sql } from './config/db.js';
+import { aj } from './lib/arcjet.js';
 const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(helmet());
 app.use(morgan("dev"));
-app.get('/test', (request, response) => {
-    console.log(response.getHeaders());
-    return response.send('<h1>Hello world!!!</h1>');
+app.use(async (request, response, next) => {
+    try {
+        const decition = await aj.protect(request, {
+            requested: 1
+        });
+        if (decition.isDenied()) {
+            if (decition.reason.isRateLimit()) {
+                response.status(429).json({
+                    error: 'Rate limit exceeded,Too many requests!!'
+                });
+            }
+            else if (decition.reason.isBot()) {
+                response.status(403).json({
+                    error: 'Bot access denied!!'
+                });
+            } else {
+                response.status(403).json({
+                    error: 'Forbidden'
+                });
+            }
+            return;
+        }
+        if (decition.results.some((result) => result.reason.isBot() && result.reason.isSpoofed())) {
+            response.status(403).json({
+                error: 'Spoofed bot detected!!'
+            });
+            return;
+        }
+        next();
+    } catch (error) {
+        console.log(error.message);
+        next(error);
+    }
 });
 app.use('/api/products', productsRoutes);
-
-
 const initDb = async () => {
     try {
         await sql`CREATE TABLE IF NOT EXISTS products (
